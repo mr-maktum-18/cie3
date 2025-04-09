01ae8ad8bd24b2b1f7f7ce6c4fb5d7f942a50dd5
mr-maktum-18 <maktumgudubhai@gmail.com>
2025-04-09 10:28:23 +0530

    save

---
+++ java.java
@@ -0,0 +1,18 @@
+public class java {
+    public static void main(String[] args) {
+        int num=10;
+        
+        if(num >= 1)
+        {
+            System.out.println("positive");
+        }
+        else if(num == 0)
+        {
+            System.out.println("positive");
+        }
+        else
+        {
+            System.out.println("negative");
+        }
+     }
+}
